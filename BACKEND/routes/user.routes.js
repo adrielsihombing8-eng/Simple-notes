b0b9ee0d7a9105842460e681c03ec74a345a91a5
adrielsihombing-sv <adrielsihombing@student.ub.ac.id>
2026-07-17 00:45:46 +0700

    Branch khusus frontend: hapus folder backend

--- BACKEND/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const userController = require('../controller/user.controller');
-const express = require('express').Router();
-
-express.post('/register', userController.register);
-express.post('/login',userController.login)
-express.get('/auth', userController.auth);
-express.post('/refresh', userController.refreshToken);
-
-module.exports = express;
--- BACKEND/routes/userdatas.routes.js
+++
@@ -1,8 +0,0 @@
-const UserDatasController = require('../controller/userdatas.controller');
-const userdatasModel = require('../model/userdatas.model');
-const express = require ('express').Router();
-
-express.post('/userBio', UserDatasController.auth, UserDatasController.saveUser);
-express.get('/getUserBio', UserDatasController.auth,)
-
-module.exports = express;
